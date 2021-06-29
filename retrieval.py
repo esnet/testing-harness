27db28a45f4ab01e41396478a8934a657e398a04
Eashan Adhikarla <aeashan@gmail.com>
2021-06-29 15:43:25 -0400

    reading jobmeta iter_uuids

--- retrieval.py
+++ retrieval.py
@@ -4,7 +4,7 @@
 ** Mentor: Ezra Kissel
 **
 ** Date Created: June 17' 2021
-** Last Modified: June 24' 2021
+** Last Modified: June 29' 2021
 **
 '''
 
@@ -18,6 +18,7 @@ import json
 import argparse
 import logging
 import errno
+import re, datetime
 
 
 try:
@@ -26,7 +27,7 @@ except OSError as e:
     if e.errno != errno.EEXIST:
         raise
 
-logging.basicConfig(filename='data/iperf3.log', level=logging.DEBUG)
+# logging.basicConfig(filename='data/iperf3.log', level=logging.DEBUG)
 
 # Create the elasticsearch client
 HOST = 'nersc-tbn-6.testbed100.es.net'
@@ -45,10 +46,10 @@ es = Elasticsearch( host=HOST, port=PORT,
 
 if not es.ping():
     raise ValueError("Connection failed")
-    logging.error("Connection failed")
+    # logging.error("Connection failed\n")
 else:
     print("Connection successful!")
-    logging.info("Connection successful!")
+    # logging.info("Connection successful!\n")
 
 
 class clr:
@@ -78,152 +79,175 @@ class GETTER:
         indices_dict = es.indices.get_alias(self.term)
         if isinstance(indices_dict, dict) and indices_dict is not None:
             print(f"'{clr.G}{len(indices_dict)}{clr.E}' indexes found!")
-            logging.info(f"'{clr.G}{len(indices_dict)}{clr.E}' indexes found!")
+            # logging.info(f"'{clr.G}{len(indices_dict)}{clr.E}' indexes found!")
             for k,v in indices_dict.items():
                 idx.append(k)
             return idx
         else:
             print (f"{clr.F}Empty dict!{clr.E}")
-            logging.info(f"{clr.F}Empty dict!{clr.E}")
+            # logging.info(f"{clr.F}Empty dict!{clr.E}")
 
-    def getIndexDetails(self, indexes, column_list, total_docs=10):
+    def getIndexDetails(self, iperf3, jobmeta, column_list, total_docs=1000):
         df = pd.DataFrame(columns=column_list)
-        for i in range(1): # len(indexes)):
+
+        for i in range(len(jobmeta)):
             try:
-                # Given a index name, finds all the documents in the index
+                # Given a index name, finds all the iperf3_documents in the index
                 # index: Index name as a string
                 # body: Empty equivalent to '*' to get all the values
-                # size: To some random max value for getting all the documents
-                result = es.search(index=indexes[i],
+                # size: To some random max value for getting all the iperf3_documents
+                jobmeta_result = es.search(index=jobmeta[i],
                                 body={"query":{"match_all":{}}},
                                 size=total_docs,
                                 )
-                print(f"\n{indexes[i]} ---> {clr.G}{result['hits']['total']['value']}{clr.E} documents\n")
-                logging.info(f"\n{indexes[i]} ---> {clr.G}{result['hits']['total']['value']}{clr.E} documents\n")
+                # print(f"\n{jobmeta[i]} ---> {clr.G}{jobmeta_result['hits']['total']['value']}{clr.E} documents\n")
+                # logging.info(f"\n{jobmeta[i]} ---> {clr.G}{jobmeta_result['hits']['total']['value']}{clr.E} documents\n")
+                jobmeta_documents = [doc for doc in jobmeta_result['hits']['hits']]
 
-                documents = [doc for doc in result['hits']['hits']]
+                for j in range(len(iperf3)):
+                    iperf3_result = es.search(index=iperf3[j],
+                                        body={"query":{"match_all":{}}},
+                                        size=total_docs,
+                                        )
+                    # print(f"\n{iperf3[i]} ---> {clr.G}{iperf3_result['hits']['total']['value']}{clr.E} documents\n")
+                    # logging.info(f"\n{iperf3[i]} ---> {clr.G}{iperf3_result['hits']['total']['value']}{clr.E} documents\n")
+                    iperf3_documents = [doc for doc in iperf3_result['hits']['hits']]
 
-                for j in range(len(documents)):
-                    # ---------------------
-                    # For each job/document
-                    # ---------------------
-                    # Format: dict_keys(['end', '@version', 'start', 'uuid', 'intervals', '@timestamp'])
-                    # uuid
-                    uuid = documents[j]['_source']['uuid']
-                    # @timestamp
-                    timestamp = documents[j]['_source']['@timestamp']
-                    # start (Format: dict_keys(['cookie', 'test_start', 'tcp_mss_default', 'version', 'connected', 'sndbuf_actual',
-                    #                           'rcvbuf_actual', 'sock_bufsize', 'system_info', 'timestamp', 'connecting_to']))
-                    start_dict = documents[j]['_source']['start']
-                    num_streams = start_dict['test_start']['num_streams']
+                    for iperfdoc in range(len(iperf3_documents)):
+                        # ---------------------
+                        # For each job/document
+                        # ---------------------
+                        # Format: dict_keys(['end', '@version', 'start', 'uuid', 'intervals', '@timestamp'])
+                        # uuid
+                        uuid = iperf3_documents[iperfdoc]['_source']['uuid']
+                        for jobmetadoc in range(len(jobmeta_documents)):
+                            try:
+                                iter_uuid = jobmeta_documents[jobmetadoc]['_source']['iter_uuids']
+                            except:
+                                pass
+                            if uuid in iter_uuid:
+                                # Hostname
+                                hostname = jobmeta_documents[jobmetadoc]['_source']['hostname']
 
-                    # ---------------------
-                    # For each stream/flow
-                    # ---------------------
-                    # Intervals
-                    intervals_list = documents[j]['_source']['intervals']
-                    for k in range(len(intervals_list)):
-                        for m in range(num_streams):
-                            # Format: dict_keys(['receiver', 'sender'])
-                            # 'sender': dict_keys(['retransmits', 'max_rtt', 'sender', 'start', 'bytes', 'mean_rtt', 'end'
-                            #                      'max_snd_cwnd', 'bits_per_second', 'socket', 'seconds', 'min_rtt'])
-                            # 'receiver': dict_keys(['end', 'bits_per_second', 'sender', 'start', 'socket', 'seconds', 'bytes'])
+                                # @timestamp
+                                timestamp = iperf3_documents[iperfdoc]['_source']['@timestamp']
+                    
+                                # start (Format: dict_keys(['cookie', 'test_start', 'tcp_mss_default', 'version', 'connected', 'sndbuf_actual',
+                                #                           'rcvbuf_actual', 'sock_bufsize', 'system_info', 'timestamp', 'connecting_to']))
+                                start_dict = iperf3_documents[iperfdoc]['_source']['start']
+                                num_streams = start_dict['test_start']['num_streams']
 
-                            sender_start = documents[j]['_source']['end']['streams'][m]['sender']['start']
-                            sender_end = documents[j]['_source']['end']['streams'][m]['sender']['end']
-                            sender_retransmits = documents[j]['_source']['end']['streams'][m]['sender']['retransmits']
-                            sender_bytes = documents[j]['_source']['end']['streams'][m]['sender']['bytes']
-                            sender_min_rtt = documents[j]['_source']['end']['streams'][m]['sender']['min_rtt']
-                            sender_max_rtt = documents[j]['_source']['end']['streams'][m]['sender']['max_rtt']
-                            sender_mean_rtt = documents[j]['_source']['end']['streams'][m]['sender']['mean_rtt']
-                            sender_bps = documents[j]['_source']['end']['streams'][m]['sender']['bits_per_second']
+                                # Congestion Type
+                                sender_congestion = iperf3_documents[iperfdoc]['_source']['end']['sender_tcp_congestion']
+                                receiver_congestion = iperf3_documents[iperfdoc]['_source']['end']['receiver_tcp_congestion']
 
-                            receiver_start = documents[j]['_source']['end']['streams'][m]['receiver']['start']
-                            receiver_end = documents[j]['_source']['end']['streams'][m]['receiver']['end']
-                            receiver_seconds = documents[j]['_source']['end']['streams'][m]['receiver']['seconds']
-                            receiver_bytes = documents[j]['_source']['end']['streams'][m]['receiver']['bytes']
-                            receiver_bps = documents[j]['_source']['end']['streams'][m]['receiver']['bits_per_second']
+                                # ---------------------
+                                # For each stream/flow
+                                # ---------------------
+                                # Intervals
+                                intervals_list = iperf3_documents[iperfdoc]['_source']['intervals']
+                                for itrv in range(len(intervals_list)):
+                                    for streams in range(num_streams):
+                                        # Format: dict_keys(['receiver', 'sender'])
+                                        # 'sender': dict_keys(['retransmits', 'max_rtt', 'sender', 'start', 'bytes', 'mean_rtt', 'end'
+                                        #                      'max_snd_cwnd', 'bits_per_second', 'socket', 'seconds', 'min_rtt'])
+                                        # 'receiver': dict_keys(['end', 'bits_per_second', 'sender', 'start', 'socket', 'seconds', 'bytes'])
 
-                            # print(f"uuid: {uuid}\ntimestamp: {timestamp}\nnum_streams: {num_streams}\nsender_start: {sender_start}\n sender_end: {sender_end}\n sender_retransmits: {sender_retransmits}\nsender_bytes: {sender_bytes}\n sender_min_rtt: {sender_min_rtt}\n sender_max_rtt: {sender_max_rtt}\nsender_mean_rtt: {sender_mean_rtt}\n sender_bps: {sender_bps}\n receiver_start: {receiver_start}\nreceiver_end: {receiver_end}\n receiver_seconds: {receiver_seconds}\n receiver_bytes: {receiver_bytes}\nreceiver_bps: {receiver_bps}\n\n")
-                            logging.info(f"uuid: {uuid}\ntimestamp: {timestamp}\nnum_streams: {num_streams}\nsender_start: {sender_start}\n sender_end: {sender_end}\n sender_retransmits: {sender_retransmits}\nsender_bytes: {sender_bytes}\n sender_min_rtt: {sender_min_rtt}\n sender_max_rtt: {sender_max_rtt}\nsender_mean_rtt: {sender_mean_rtt}\n sender_bps: {sender_bps}\n receiver_start: {receiver_start}\nreceiver_end: {receiver_end}\n receiver_seconds: {receiver_seconds}\n receiver_bytes: {receiver_bytes}\nreceiver_bps: {receiver_bps}\n\n")
+                                        sender_throughput = iperf3_documents[iperfdoc]['_source']['end']['streams'][streams]['sender']['bits_per_second']
+                                        sender_min_rtt = iperf3_documents[iperfdoc]['_source']['end']['streams'][streams]['sender']['min_rtt']/2
+                                        sender_max_rtt = iperf3_documents[iperfdoc]['_source']['end']['streams'][streams]['sender']['max_rtt']/2
+                                        sender_mean_rtt = iperf3_documents[iperfdoc]['_source']['end']['streams'][streams]['sender']['mean_rtt']/2
+                                        sender_retransmits = iperf3_documents[iperfdoc]['_source']['end']['streams'][streams]['sender']['retransmits']
+                                        receiver_throughput = iperf3_documents[iperfdoc]['_source']['end']['streams'][streams]['receiver']['bits_per_second']
 
-                            df = df.append({'uuid':uuid,
-                                            'timestamp':timestamp,
-                                            'num_streams':num_streams,
-                                            'sender_start':sender_start,
-                                            'sender_end':sender_end,
-                                            'sender_retransmits':sender_retransmits,
-                                            'sender_bytes':sender_bytes,
-                                            'sender_min_rtt':sender_min_rtt,
-                                            'sender_max_rtt':sender_max_rtt,
-                                            'sender_mean_rtt':sender_mean_rtt,
-                                            'sender_bps':sender_bps,
-                                            'receiver_start':receiver_start,
-                                            'receiver_end':receiver_end,
-                                            'receiver_seconds':receiver_seconds,
-                                            'receiver_bytes':receiver_bytes,
-                                            'receiver_bps':receiver_bps
-                                            }, ignore_index=True)
+                                        # print(f"uuid: {uuid}\nhostname: {hostname}\ntimestamp: {timestamp}\nnum_streams: {num_streams}\nsender_throughput: {sender_throughput}\nreceiver_throughput: {receiver_throughput}\nsender_min_rtt: {sender_min_rtt}\nsender_max_rtt: {sender_max_rtt}\nsender_mean_rtt: {sender_mean_rtt}\nsender_congestion: {sender_congestion}\nreceiver_congestion: {receiver_congestion}\nsender_retransmits: {sender_retransmits}\n\n")
+                                        # logging.info(f"uuid: {uuid}\nhostname: {hostname}\ntimestamp: {timestamp}\nnum_streams: {num_streams}\nsender_throughput: {sender_throughput}\nreceiver_throughput: {receiver_throughput}\nsender_min_rtt: {sender_min_rtt}\nsender_max_rtt: {sender_max_rtt}\nsender_mean_rtt: {sender_mean_rtt}\nsender_congestion: {sender_congestion}\nreceiver_congestion: {receiver_congestion}\nsender_retransmits: {sender_retransmits}\n\n")
 
+                                        df = df.append({'UUID':uuid,
+                                                        'HOSTNAME':hostname,
+                                                        'TIMESTAMP':timestamp,
+                                                        'STREAMS':num_streams,
+                                                        'THROUGHPUT (Sender)':sender_throughput,
+                                                        'THROUGHPUT (Receiver)':receiver_throughput,
+                                                        'LATENCY (min.)':sender_min_rtt,
+                                                        'LATENCY (max.)':sender_max_rtt,
+                                                        'LATENCY (mean)':sender_mean_rtt,
+                                                        'CONGESTION (Sender)':sender_congestion,
+                                                        'CONGESTION (Receiver)':receiver_congestion,
+                                                        'RETRANSMITS':sender_retransmits,
+                                                        }, ignore_index=True)                                    
             except:
                 pass
-            # print("\nTotal docs found: ", self.sum)
-            print("Done parsing!")
         return df
 
 
-# class PLOT:
-#     def __init__(self):
-#         self.
-
-#     def rtt(self): # By algorithm type {cubic, bbrv2} - plot mean, min, max
-#         return NotImplemented
-
-#     def bps(self): # By algorithm type {cubic, bbrv2}
-#         return NotImplemented
-
-
-def main():
+def main(verbose=False):
     print("\nStarting ELK testpoint stats retrieval...")
-    logging.info("\nStarting ELK testpoint stats retrieval...")
+    # logging.info("\nStarting ELK testpoint stats retrieval...")
 
     parser = argparse.ArgumentParser(description='Testpoint Statistics')
-    parser.add_argument('-t', '--term', default="iperf3*", type=str,
+    parser.add_argument('-t', '--term', default="*", type=str,
                         help='The search term to find the indexes {"*", "iperf3*", "jobmeta*", "bbrmon*"}')
     args = parser.parse_args()
 
-    # indexTypes = ["*", "iperf3*", "jobmeta*", "bbrmon*"]
-    # term_ = indexTypes[1]
-
     print(f"Chosen index type: {args.term}")
-    logging.info(f"Chosen index type: {args.term}")
+    # logging.info(f"Chosen index type: {args.term}")
     get = GETTER(args.term)
 
     # STEP 1. Get all the indices in the ELK given a term.
     indexes = get.getIndexList(args.term)
-    for e,i in enumerate(indexes):
-        print(f"{e}: {i}")
-        logging.info(f"{e}: {i}")
 
-    pandas_column_list = ['uuid',
-                          'timestamp',
-                          'num_streams',
-                          'sender_start','sender_end','sender_retransmits','sender_bytes','sender_min_rtt','sender_max_rtt','sender_mean_rtt','sender_bps',
-                          'receiver_start','receiver_end','receiver_seconds','receiver_bytes','receiver_bps',
-                         ]
+    if verbose:
+        for e,i in enumerate(indexes):
+            print(f"{e}: {i}")
+            # logging.info(f"{e}: {i}")
+
+    # Filtering indexes by type
+    sorted_index = sorted(indexes)
+    iperf3 = [i for i in sorted_index if i.startswith("iperf3")]
+    jobmeta = [i for i in sorted_index if i.startswith("jobmeta")]
+    ss = [i for i in sorted_index if i.startswith("ss")]
+    bbrmon_pscheduler = [i for i in sorted_index if i.startswith("bbrmon-pscheduler")]
+    bbrmon_jobmeta = [i for i in sorted_index if i.startswith("bbrmon-jobmeta")]
+    bbrmon_ss = [i for i in sorted_index if i.startswith("bbrmon-ss")]
+    bbrmon_tcptrace = [i for i in sorted_index if i.startswith("bbrmon-tcptrace")]
+    miscellaneous = [i for i in sorted_index if i not in (iperf3+jobmeta+ss+bbrmon_jobmeta+bbrmon_pscheduler+bbrmon_ss+bbrmon_tcptrace)]
+    
+    flag = len(iperf3+jobmeta+ss+bbrmon_jobmeta+bbrmon_pscheduler+bbrmon_ss+bbrmon_tcptrace+miscellaneous) == len(sorted_index)
+    if not flag:
+        print("New indexes got added, missing in the working list!")
+        # logging.info("New indexes got added, missing in the working list!")
+        missing = sorted_index - (iperf3+jobmeta+ss+bbrmon_jobmeta+bbrmon_pscheduler+bbrmon_ss+bbrmon_tcptrace+miscellaneous)
+        for e,i in enumerate(indexes):
+            print(f"{e}: {i}")
+            # logging.info(f"{e}: {i}")
+
+    pandas_column_list = [
+                        'UUID',
+                        'HOSTNAME',
+                        'TIMESTAMP',
+                        'STREAMS',
+                        'THROUGHPUT (Sender)', 'THROUGHPUT (Receiver)',
+                        'LATENCY (min.)', 'LATENCY (max.)', 'LATENCY (mean)',
+                        'CONGESTION (Sender)', 'CONGESTION (Receiver)',
+                        'RETRANSMITS',
+                        ]
 
     # STEP 2. getIndexDetails to retrieve the statistics of every testpoint
     # and every stream/flow wrt index
-    index_response = get.getIndexDetails(indexes, pandas_column_list)
+    index_response = get.getIndexDetails(iperf3, jobmeta, pandas_column_list)
 
     # STEP 3. Create a Pandas Dataframe to make it easier for the model to read.
+    if args.term=="*":
+        filename = "all"
+    else:
+        filename = args.term[:-1]
+
     try:
-        index_response.to_csv('data/'+str(args.term[:-1])+'.csv')
-        print(f"{str(args.term[:-1])}.csv file written!")
+        index_response.to_csv('data/'+str(filename)+'.csv')
+        print(f"{str(filename)}.csv file written!")
     except:
         raise ValueError("Cannot write the file")
-
-    # STEP 4. Plot some of the data for sanity checks and analysis 
 
 
 if __name__ == "__main__":
