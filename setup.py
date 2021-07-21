ad668d2d92d87fe436485c6bdc4810ec2f1047c4
Ezra Kissel <kissel@es.net>
2021-07-21 14:08:08 -0400

    Add a setup.py to aid in dependency tracking.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='testing-harness',
+    version='0.1',
+    description='ESnet Testing Harness',
+    url='https://github.com/esnet/testing-harness',
+    author='Ezra Kissel',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'License :: OSI Approved :: Apache License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+
+    keywords='testing harness protocol iperf3 perfsonar',
+
+    packages=find_packages(),
+
+    install_requires=['requests', 'pyyaml', 'pika']
+)
