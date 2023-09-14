b4497ecd580bdb8d8ed6eba17e5c67859371f7ef
Brian Tierney <bltierney@gmail.com>
2023-09-14 15:53:38 +0000

    updates

--- utils/post-src-pacing.sh
+++ utils/post-src-pacing.sh
@@ -3,12 +3,10 @@ set -x
 
 #eth=ens3f1
 #eth=p2p1
-eth=eth200
+eth=dtn1
 
 # to reset to default:
 /sbin/tc qdisc del dev $eth root
 
-/usr/sbin/sysctl -w net.ipv4.tcp_congestion_control=bbr2 
-
-rm -f /usr/bin/iperf3; ln -s /usr/bin/iperf3-dist /usr/bin/iperf3
+#rm -f /usr/bin/iperf3; ln -s /usr/bin/iperf3-dist /usr/bin/iperf3
 
