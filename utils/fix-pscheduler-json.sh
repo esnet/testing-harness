aa2d1a5efed00435f7fe04bda6b82cf12e6c7814
Brian Tierney <tierney@jhkang-dev-8.es.net>
2024-11-13 17:57:19 +0000

    new script added

---
+++ utils/fix-pscheduler-json.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# JSON output from pscheduler is a mess, and not parsable. This script will clean it up.
+
+# Check if input file is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <inputfile>"
+  exit 1
+fi
+
+inputfile="$1"
+outputfile="${inputfile}.fixed.json"
+
+# Step 1: Replace escape sequences and save to test.out
+sed -e 's/\\n/\n/g' -e 's/\\"/"/g' "$inputfile" > tmp.out
+
+# Step 2: Remove the first 3 lines and lines from 'Participant' to the end, and save to fixed.out
+sed -e '1,3d' -e '/Participant/,$d' tmp.out > "$outputfile"
+
+# for debugging: Extract the .start and .end.sum_sent fields from the JSON in fixed.out
+#start=$(jq .start "$outputfile")
+end_sum_sent=$(jq .end.sum_sent "$outputfile")
+
+# Display some of the results
+#echo "Start: $start"
+echo "End Sum Sent: $end_sum_sent"
+
+rm tmp.out
+
+
